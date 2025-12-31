2ed7fda66a8ba162b49501a96db9c031e6b50588
tuxedo-synced <rahul.synced@gmail.com>
2025-12-31 16:41:06 +0530

    added how to detect a csv and json file using logical operator and .endswith method

--- Files/File_Detection.py
+++ Files/File_Detection.py
@@ -1,12 +1,16 @@
 import os
 
-file_paths = ["Files/test.txt","vs code notes.txt","something.pdf",r"C:\Users\rahul\OneDrive\Documents\dbms\DBMS REFERENCE PDF.pdf",r"C:\Users\rahul\OneDrive\Documents\ms excel and power bi\final project"]
-# \U  \D  \O  -> have some other use so to avoid that conflict we place r before double quotes meas 'read it as is'
+file_paths = ["Files/test.txt","Files/test.csv","something.pdf",r"C:\Users\rahul\OneDrive\Documents\dbms\DBMS REFERENCE PDF.pdf",r"C:\Users\rahul\OneDrive\Documents\ms excel and power bi\final project"]
+# \U  \D  \O  -> have some other use so to avoid that conflict we place r before double quotes means 'read it as is'
 for file_path in file_paths:
     if os.path.exists(file_path):
         print(f"The '{file_path}' allocation exists .")
         if os.path.isfile(file_path):
             print("Its a file .")
+        if os.path.isfile(file_path) and file_path.endswith(".csv"):
+            print("Its a csv file .")
+        if os.path.isfile(file_path) and file_path.endswith(".json"):
+            print("Its a json file .")     
         else:
             print("Its not a file .")
     else:
